640eb5f1a5c38caa2e25dc2e6ea26a08c812909f
Kim._.jazz <jeong011010@naver.com>
2025-07-21 16:25:19 +0900

    fix: 지도 상세정보 카드 디자인 수정

--- src/components/MapStoreCard.tsx
+++ src/components/MapStoreCard.tsx
@@ -16,17 +16,17 @@ export default function MapStoreCard({ store }: { store: Store }) {
 
       {/* 매장 정보 */}
       <div className="space-y-1 px-4">
-        <h2 className="font-bold text-gray-900 py-4 text-xl">{store.name}</h2>
+        <h2 className="font-bold text-gray-900 py-1 text-xl">{store.name}</h2>
         <p className="text-sm text-gray-400">{store.address}</p>
         <p className="text-sm text-gray-400">{store.phone || '전화번호 미등록'}</p>
-        <p className="text-sm pt-6 text-red-400 font-semibold">
+        <p className="text-sm pt-4 text-red-400 font-semibold">
           {isStoreOpen(store.hours, store.remain) ? '영업 중' : '영업 종료'}
         </p>
         <p className="text-sm pb-2 text-gray-400">천원의 아침밥 운영 시간: {store.hours}</p>
       </div>
 
       {/* 천원의 아침밥 */}
-      <div className="mt-4 bg-orange-500 text-white px-4 py-5 flex justify-between items-center">
+      <div className="mt-4 bg-orange-500 text-white px-4 py-3 flex justify-between items-center">
         <div>
           <p className="font-semibold text-lg">오늘의 천밥</p>
           <p className="text-sm">{store.menu.join(', ')}</p>
