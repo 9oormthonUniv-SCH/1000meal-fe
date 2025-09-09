2800d3887a5438d6fe554135d30382ee41017a0b
Kim._.jazz <jeong011010@naver.com>
2025-09-09 18:26:18 +0900

    fix: pwa 로고 변경 & ga 환경변수 ci 추가

--- src/app/_pwa-head.tsx
+++ src/app/_pwa-head.tsx
@@ -9,8 +9,8 @@ export function PwaHead() {
       {/* iOS PWA */}
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-      <link rel="apple-touch-icon" href="/icons/pwa180-2.png" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/icons/pwa180-2.png" />
+      <link rel="apple-touch-icon" href="/icons/logo.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/icons/logo.png" />
 
       {/* iOS 전용 스플래시 */}
       <link rel="apple-touch-startup-image" media="screen and (device-width: 440px) and (device-height: 956px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)" href="/splash_screens/iPhone_16_Pro_Max_landscape.png"/>
