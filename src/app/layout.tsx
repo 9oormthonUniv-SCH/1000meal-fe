d3de7ca7c06fe65e06853f98123c6e7ae506e7f2
Kim._.jazz <jeong011010@naver.com>
2025-09-09 18:44:07 +0900

    fix: ga 로직 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { PwaHead } from "./_pwa-head";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "천원의 아침밥",
+  title: "오늘 순밥",
   description: "대학생을 위한 아침 식사 정보 서비스",
 };
 
@@ -22,6 +22,20 @@ export default function RootLayout({
           src={`https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_KEY}&autoload=false&libraries=services`}
           strategy="beforeInteractive"
         />
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="ga-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+              page_path: window.location.pathname,
+            });
+          `}
+        </Script>
       </head>
       <body className="bg-gray-200">
         <div className="flex justify-center">
